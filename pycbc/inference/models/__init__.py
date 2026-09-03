79ffaa1cb2d85c6f8bc3b2c7526996fbb42673fd
Vikas Jadhav <vikasjadhav@Vikass-MacBook-Air.local>
2026-09-03 16:32:23 -0400

    Added the model name to the list of models in the __init__ file

--- pycbc/inference/models/__init__.py
+++ pycbc/inference/models/__init__.py
@@ -32,6 +32,7 @@ from .marginalized_gaussian_noise import MarginalizedPhaseGaussianNoise
 from .marginalized_gaussian_noise import MarginalizedPolarization
 from .marginalized_gaussian_noise import MarginalizedHMPolPhase
 from .marginalized_gaussian_noise import MarginalizedTime
+from .marginalized_gaussian_noise import AnalyticHMPolPhase
 from .brute_marg import BruteParallelGaussianMarginalize
 from .brute_marg import BruteLISASkyModesMarginalize
 from .gated_gaussian_noise import (GatedGaussianNoise, GatedGaussianMargPol)
@@ -209,6 +210,7 @@ _models = {_cls.name: _cls for _cls in (
     MultiSignalModel,
     RelativeTimeDom,
     JointPrimaryMarginalizedModel,
+    AnalyticHMPolPhase
 )}
 
 
